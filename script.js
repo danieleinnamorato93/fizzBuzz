4c9334baca18820b6ea45cfae00e98e6ada71de7
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2025-01-16 17:01:55 +0100

    print in page

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+//!Recupero la lista
+const list = document.getElementById("numbers-list");
+let items = "";
+
 for (let i = 1; i <= 100; i++) {
   //console.log(i);
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
+    items += `<li>FizzBuzz</li>`;
   } else if (i % 3 === 0) {
-    console.log("Fizz");
+    items += `<li>Fizz</li>`;
   } else if (i % 5 === 0) {
-    console.log("Buzz");
+    items += `<li>Buzz</li>`;
   } else {
-    console.log(i);
+    items += `<li>${i}</li>`;
   }
 }
+
+//! Inserisco i risultati nella lista in pagina
+list.innerHTML = items;
